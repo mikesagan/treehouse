25109325debae7f498a88279920924d060da75d8
mikesagan <mike02421@aol.com>
2013-12-23 12:58:06 -0700

    Created the devise user login

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,4 +26,8 @@ Treehouse::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+
+  #This is the configuration links for Devise
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Treehouse::Application.routes.draw do
+  devise_for :users
+  resources :users
+
   resources :statuses
+
+  root to: 'statuses#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
